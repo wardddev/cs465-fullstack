c6d73bb50edb31a29bc2949106e29c8fa8395740
wardddev <wardd.dev@outlook.com>
2024-10-18 21:26:55 -0700

    Module 6 Completed Baseline

--- travlr/app.js
+++ travlr/app.js
@@ -17,6 +17,12 @@ app.set('views', path.join(__dirname, 'app_server', 'views'));
 // register handlebars partials (https://www.npmjs.com/package/hbs)
 handlebars.registerPartials(__dirname + '/app_server/views/partials');
 
+
+
+// missing lines on pg 59 of module six
+
+
+
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -25,6 +31,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Enable CORS
+app.use('/api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
+
+// wire-up routes to controllers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
--- travlr/app_admin/src/app/app.component.ts
+++ travlr/app_admin/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TripListingComponent } from './trip-listing/trip-listing.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TripListingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'travlr-admin';
+  title = 'Travlr Getaways Admin!';
 }
