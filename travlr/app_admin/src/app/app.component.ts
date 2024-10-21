7a90d3ad079bfd0440dbda1658e58151b5f5d79a
wardddev <wardd.dev@outlook.com>
2024-10-21 02:08:34 -0700

    Updated app.component.html
    added navbar, cleaned up
    
    updated formatting in others

--- travlr/app_admin/src/app/app.component.ts
+++ travlr/app_admin/src/app/app.component.ts
@@ -5,10 +5,11 @@ import { TripListingComponent } from './trip-listing/trip-listing.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TripListingComponent],
+  imports: [TripListingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'Travlr Getaways Admin';
 }
