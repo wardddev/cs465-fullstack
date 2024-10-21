5543f88d09ebf42ea3ff7271d68882653208e34e
wardddev <wardd.dev@outlook.com>
2024-10-21 02:53:02 -0700

    Update trip-listing.component.ts
    Import TripCardComponent

--- travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
+++ travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { trips } from '../data/trips';
 import { CommonModule } from '@angular/common';
+import { trips } from '../data/trips';
+import { TripCardComponent } from '../trip-card/trip-card.component';
 
 
 @Component({
   selector: 'app-trip-listing',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TripCardComponent],
   templateUrl: './trip-listing.component.html',
   styleUrl: './trip-listing.component.css'
 })
+
 export class TripListingComponent implements OnInit {
   trips: Array<any> = trips;
 
