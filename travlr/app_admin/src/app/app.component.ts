283ff0e94e47ad11a84c83a7665ed725fbb899d6
wardddev <wardd.dev@outlook.com>
2024-10-21 00:57:54 -0700

    Import TripListingComponent into AppComponent
    step 4

--- travlr/app_admin/src/app/app.component.ts
+++ travlr/app_admin/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TripListingComponent } from './trip-listing/trip-listing.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TripListingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
