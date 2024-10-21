d8f0bc0ac9a7dde63cdb8c508bd19640ae18a6e2
wardddev <wardd.dev@outlook.com>
2024-10-21 03:19:29 -0700

    Update trip-data.service.ts
    step 5
    implement trip model

--- travlr/app_admin/src/app/services/trip-data.service.ts
+++ travlr/app_admin/src/app/services/trip-data.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs'; // handles async calls
+
+import { Trip } from '../models/trip';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TripDataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTrips() : Observable<Trip[]> {
+    let url = 'http://localhost:3000/api/trips';
+
+    return this.http.get<Trip[]>(url);
+  }
 }
