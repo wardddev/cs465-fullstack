50f63be3da8de55b298e637736d844e48886e288
wardddev <wardd.dev@outlook.com>
2024-10-24 03:16:53 -0700

    Update user.js

--- travlr/app_api/models/user.js
+++ travlr/app_api/models/user.js
@@ -15,3 +15,34 @@ const userSchema = new mongoose.Schema({
 });
 
 mongoose.model('users', userSchema);
+
+// enable cryptography aspect and JSON web tokens
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
+// Method to set the password on this record.
+userSchema.methods.setPassword = function(password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+};
+
+// Method to compare entered password against stored hash
+userSchema.methods.validPassword = function(password) {
+    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+    return this.hash === hash;
+}
+
+// Method to generate a JSON Web Token for the current record
+userSchema.methods.generateJWT = function() {
+    return jwt.sign(
+    { // Payload for our JSON Web Token
+        _id: this._id,
+        email: this.email,
+        name: this.name,
+    },
+    process.env.JWT_SECRET, // SECRET stored in .env file
+    { expiresIn: '1h' }); // Token expires an hour from creation
+};
+
+const User = mongoose.model('users', userSchema);
+module.exports = User;
