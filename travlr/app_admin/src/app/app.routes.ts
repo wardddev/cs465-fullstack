35e7fab0c10b3dcadda32141240fc017c9889821
wardddev <wardd.dev@outlook.com>
2024-10-23 23:02:21 -0700

    Update app.routes.ts
    step 6
    Set up routing for AddTrip and TripListing

--- travlr/app_admin/src/app/app.routes.ts
+++ travlr/app_admin/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { AddTripComponent } from './add-trip/add-trip.component';
+import { TripListingComponent } from './trip-listing/trip-listing.component';
+
+export const routes: Routes = [
+    { path: 'add-trip', component: AddTripComponent },
+    { path: '', component: TripListingComponent, pathMatch: 'full' }
+];
