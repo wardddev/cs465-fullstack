7bc6a84696e7d0512eb18b37b95e5102b3cacca6
wardddev <wardd.dev@outlook.com>
2024-11-14 03:11:43 -0800

    added function for backend email validation

--- travlr/app_api/controllers/mailing.js
+++ travlr/app_api/controllers/mailing.js
@@ -1,17 +1,25 @@
 const nodemailer = require('nodemailer');
+const validator = require('validator');
+
+//require('dotenv').config();
 
 // Use test SMTP account mailtrap for capturing test emails
 let transporter = nodemailer.createTransport({
     host: 'sandbox.smtp.mailtrap.io', // SMTP host
     port: 2525, // mailtrap
     auth: {
-        user: '3b9335354c9853', // SMTP usernamer
+        user: '3b9335354c9853', // SMTP username
         pass: 'd007592d1b1a2b' // SMTP password
     }
 });
 
 async function sendEmail(req, res) {
     const userEmail = req.body.email;
+
+    // validates email format
+    if (!validator.isEmail(userEmail)) {
+        return res.status(400).send({ error: 'Invalid email format' });
+    }
 
     try {
         let info = await transporter.sendMail({
