44f04fcf8bee062f8756837d056d1c41aa86d366
wardddev <wardd.dev@outlook.com>
2024-11-22 21:16:33 -0800

    Updated naming

--- travlr/public/javascripts/dropdownmenu.js
+++ travlr/public/javascripts/dropdownmenu.js
@@ -1,5 +1,5 @@
 /* When the user clicks on the button, toggle between hiding and showing the dropdown content */
-function sorting() {
+function dropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
