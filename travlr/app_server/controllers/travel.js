e29689e3e9122f0e4898c937661f7c95b838cf60
wardddev <wardd.dev@outlook.com>
2024-11-22 20:50:18 -0800

    Update travel.js

--- travlr/app_server/controllers/travel.js
+++ travlr/app_server/controllers/travel.js
@@ -1,36 +1,47 @@
-const tripsEndpoint = 'http://localhost:3000/api/trips';
+const tripsEndpoint = process.env.TRIPS_API_ENDPOINT || 'http://localhost:3000/api/trips';
 const options = {
     method: 'GET',
     headers: {
         'Accept': 'application/json'
     }
-}
+};
 
-// var fs = require('fs');
-// var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+/* Fetch Trips from API */
+const fetchTrips = async () => {
+    const response = await fetch(tripsEndpoint, options);
+    if (!response.ok) {
+        throw new Error(`Failed to fetch trips: ${response.status} ${response.statusText}`);
+    }
+    return await response.json();
+};
 
 /* GET Travel view */
 const travel = async function(req, res, next) {
-    // console.log('TRAVEL CONTROLLER BEGIN');
-    await fetch(tripsEndpoint, options)
-        .then(res => res.json())
-        .then(json => {
-            // console.log(json);
-            let message = null;
-            if(!(json instanceof Array)) {
-                message = 'API lookup error';
-                json = [];
-            } else {
-                if(!json.length) {
-                    message = 'No trips exist in our database!';
-                }
-            }
-            res.render('travel', {title: 'Travlr Getaways', trips: json, message});
-        })
-        .catch(err => res.status(500).send(e.message));
-    // console.log('TRAVEL CONTROLLER AFTER RENDER');
+    try {
+        const trips = await fetchTrips();
+        let message = null;
+
+        if (!(trips instanceof Array)) {
+            message = 'API lookup error';
+        } else if (!trips.length) {
+            message = 'No trips exist in our database!';
+        }
+
+        res.render('travel', {
+            title: 'Travlr Getaways',
+            trips: trips || [],
+            message
+        });
+    } catch (err) {
+        console.error('Error fetching trips:', err.message);
+        res.status(500).render('travel', {
+            title: 'Travlr Getaways',
+            trips: [],
+            message: 'An error occurred while fetching trips. Please try again later.'
+        });
+    }
 };
 
 module.exports = {
     travel
-}
+};
